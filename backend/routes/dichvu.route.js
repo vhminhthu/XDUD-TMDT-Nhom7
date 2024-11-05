68f9523108064b6825844f16b9adf5247899571c
PhamTien-363296 <phamnguyenthuytien3007@gmail.com>
2024-11-05 23:41:49 +0700

    chinh routes

--- backend/routes/dichvu.route.js
+++ backend/routes/dichvu.route.js
@@ -1,9 +1,5 @@
 import express from "express"
-<<<<<<< HEAD
 import {layDichVu,themDichVu, suaDichVu, xoaDichVu, idDichvu} from '../controllers/dichvu.controller.js'
-=======
-import {layDichVu,idDichvu,themDichVu, suaDichVu, xoaDichVu} from '../controllers/dichvu.controller.js'
->>>>>>> 60fed8ab2de686b5bc9f928f393915fa68851bc6
 
 const router = express.Router()
 
